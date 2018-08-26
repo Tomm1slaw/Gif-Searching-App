57c9a5d715d74d815ee522feb1aea0d7334a6de4
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-08-26 20:38:26 +0200

    Fix App.js

--- components/App.js
+++ components/App.js
@@ -50,12 +50,16 @@ App = React.createClass({
 
         return ( 
             <div style = {styles}>
-            <h1> Wyszukiwarka GIFow! </h1>
-            <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
-            <Search onSearch = {this.handleSearch}/> 
-            <Gif loading = {this.state.loading} 
-            url = {this.state.gif.url}
-            sourceUrl = {this.state.gif.sourceUrl}/>
+            <h1>Wyszukiwarka GIFow!</h1>
+            <p>
+            Znajdź gifa na 
+            <a href={http://giphy.com}>giphy</a>. Naciskaj enter, aby pobrać kolejne gify.</p>
+            <Search onSearch = {this.handleSearch} /> 
+            <Gif 
+                loading = {this.state.loading} 
+                url = {this.state.gif.url}
+                sourceUrl = {this.state.gif.sourceUrl}
+            />
             </div>
         );
     }
